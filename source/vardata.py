aa8b8bf3f3ffad6347b600bb32695eeb81f9574e
tzpyrope <53115539+tzpyrope@users.noreply.github.com>
2023-05-06 15:47:54 +0200

    csv fix

--- source/vardata.py
+++ source/vardata.py
@@ -4,7 +4,7 @@ from pathlib2 import Path
 
 
 def replace_none_with_empty():
-    df = Path(r"source/events.csv")
+    df = Path(r"mitp-project/source/events.csv")
 
     data = df.read_text()
     data = data.replace("brak", "")
@@ -15,7 +15,7 @@ def replace_none_with_empty():
 
 replace_none_with_empty()
 
-df = pd.read_csv(r"source/events.csv",
+df = pd.read_csv(r"mitp-project/source/events.csv",
     delimiter = ",",
     usecols = ["Typ", "Tytuł", "Uwaga", "Pierwszy dzień", "Ostatni dzień", "Ogłoszony początek", "Ogłoszony koniec", "Miejsce"],
     dtype = None)
