069adb477221df900c4836b8802058bec3d47f2b
tzpyrope <53115539+tzpyrope@users.noreply.github.com>
2023-05-08 16:02:03 +0200

    deleted not used func

--- source/instruct_and_date_conversion.py
+++ source/instruct_and_date_conversion.py
@@ -62,25 +62,6 @@ def account_for_single_day_events(start_time_end_day: list, end_time_end_day: li
     end_time_end_day.append("N/A")
     weeks.append(0)
 
-def make_multidimensional_list_for_end_days(start_time_end_day: list, end_time_end_day: list, weeks: list, i: int): # refactor this
-    end_day = datetime.strptime(end_days[i], "%d.%m.%Y")
-    start_day = datetime.strptime(start_days[i], "%d.%m.%Y")
-    week_count = (abs(end_day - start_day).days) // 7
-    weeks.append(week_count)
-    
-    start_time_end_day.append([])
-    end_time_end_day.append([])
-
-    for number in range(0, (week_count + 1)):
-        date = start_day + timedelta(weeks=number)
-        date = datetime.strftime(date, "%d.%m.%Y")
-        date_begin = date + " " + start_time[i]
-        date_end = date + " " + end_time[i]
-        date_begin = datetime.strptime(date_begin, "%d.%m.%Y %H:%M")
-        date_end = datetime.strptime(date_end, "%d.%m.%Y %H:%M")
-        start_time_end_day[i].append(date_begin)
-        end_time_end_day[i].append(date_end)
-
 def end_days_datetime_list_conversion(): # refactor this
     start_time_end_days = []
     end_time_end_days = []
